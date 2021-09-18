30b2bf0ee1fc76919960673c95d1faac0c0fe874
Biltu Das <billionto@gmail.com>
2021-09-18 13:20:28 +0530

    Fixed checking Errors

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,5 @@
 url = window.location.href;
-url_find = url.includes("https://")
+url_find = url.includes("https")
 url_find1 = url.includes("getprivacy.cf")
 url_result = url_find + url_find1;
 if (url_result == 2) {
