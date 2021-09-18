b2856fc26ba8c6c4a900fcb461dcd96146d6bd3f
Biltu Das <billionto@gmail.com>
2021-09-18 13:07:36 +0530

    Added New Feature: Warn on Unsecure Connection

---
+++ js/helper.js
@@ -0,0 +1,9 @@
+url = window.location.href;
+url_find = url.includes("https://")
+url_find1 = url.includes("getprivacy.cf")
+url_result = url_find + url_find1;
+if (url_result == 2) {
+  document.getElementById("secur").style.visibility = "hidden";
+} else {
+  document.getElementById("secur").style.visibility = "";
+}
