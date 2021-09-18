5202e270f652be1f0952ff8c51e89044adc767f3
Biltu Das <billionto@gmail.com>
2021-09-18 15:34:25 +0530

    Added ClickJack Protection

--- js/helper.js
+++ js/helper.js
@@ -2,3 +2,8 @@ url = window.location.href;
 url_find = url.includes("https")
 url_find1 = url.includes("getprivacy.cf")
 url_result = url_find + url_find1;
+if (self === top) {
+  var antiClickjack = document.getElementById("antiClickjack");
+  antiClickjack.parentNode.removeChild(antiClickjack);
+} else {
+  top.location = self.location; }
