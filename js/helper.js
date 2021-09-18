ccbf2b2e1251444f2ec2c39400e1d9443e58a7aa
Biltu Das <billionto@gmail.com>
2021-09-18 13:29:31 +0530

    Fixed checking Errors

--- js/helper.js
+++ js/helper.js
@@ -2,8 +2,3 @@ url = window.location.href;
 url_find = url.includes("https")
 url_find1 = url.includes("getprivacy.cf")
 url_result = url_find + url_find1;
-if (url_result == 2) {
-  document.getElementById("secur").style.visibility = "hidden";
-} else {
-  document.getElementById("secur").style.visibility = "";
-}
