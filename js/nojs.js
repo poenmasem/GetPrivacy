b9e0999c9487bf9b9f244c795783b23822473d2d
Biltu Das <billionto@gmail.com>
2021-09-22 22:50:09 +0530

    Fixed No Javascript

--- js/nojs.js
+++ js/nojs.js
@@ -1,6 +1,5 @@
 url = window.location.href;
 url = url.split('?ref=')[1];
-url = "https://getprivacy.cf/" + url
 if ( url == "undefined" ) {
   window.location.replace("https://getprivacy.cf");
 } else {
